b806d3bafd2016dd4a4060cef2fb17bba1925e37
Kenan Casey <kenancasey@gmail.com>
2019-10-04 14:15:49 -0500

    initial commit

---
+++ AnimalQueue/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace AnimalQueue
+{
+    public enum AnimalType { Cat, Dog, Rabbit, NoPreference }
+
+    public class Animal
+    {
+        public Animal()
+        {
+        }
+
+        public AnimalType AnimalType { get; set; }
+
+        public string Name { get; set; }
+
+        public double Weight { get; set; }
+
+        public override string ToString()
+        {
+            
+        }
+
+    }
+}
---
+++ AnimalQueue/AnimalShelter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace AnimalQueue
+{
+    public class AnimalShelter
+    {
+        public AnimalShelter()
+        {
+        }
+
+        int TotalAnimals
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public void Add(Animal animal)
+        {
+            // add the animal to the data structure
+        }
+
+        public Animal Adopt(AnimalType animalType)
+        {
+
+        }
+
+    }
+}
